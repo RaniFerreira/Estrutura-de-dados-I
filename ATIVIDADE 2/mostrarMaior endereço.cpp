fbc5b42c81ed1f4a3ac1883769b238932e7efc15
RaniFerreira <129872650+RaniFerreira@users.noreply.github.com>
2024-09-29 21:25:27 -0300

    Add files via upload

---
+++ ATIVIDADE 2/mostrarMaior endereço.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+1. Escreva um programa que contenha duas variaveis inteiras.
+Leia essas variáveis do teclado.
+Em seguida, compare seus endereços e exiba o conteudo do maior endereço.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int var1, var2;
+    int *pont1, *pont2;
+    
+    cin >> var1 >> var2;
+    
+    pont1 = &var1;
+    pont2 = &var2;
+    
+    if(pont1 > pont2){
+        cout << "O maior endereço é: " << pont1 << endl;
+    }
+    else{
+        cout << "O maior endereço é: " << pont2 << endl;
+    }
+
+    return 0;
+}
