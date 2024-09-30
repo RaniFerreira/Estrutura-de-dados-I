1da4900b0ad4b451d3150e83dc86a50200fd7f7a
RaniFerreira <129872650+RaniFerreira@users.noreply.github.com>
2024-09-29 22:12:23 -0300

    Update mostrar Maior endereço.cpp

--- ATIVIDADE 2/mostrar Maior endereço.cpp
+++ ATIVIDADE 2/mostrar Maior endereço.cpp
@@ -8,24 +8,33 @@ Em seguida, compare seus endereços e exiba o conteudo do maior endereço.
 #include <iostream>
 using namespace std;
 
+int endereco(int *pont1, int *pont2){
+    
+    cout << "Endereços: " << endl;
+    cout << pont1 << endl;
+    cout << pont2 << endl;
+    
+    if(pont1 > pont2){
+         cout << "O maior endereço: "<< pont1 << endl <<
+        "valor guardado: " << *pont1 << endl;
+    }
+    else{
+        cout << "O maior endereço: "<< pont2 << endl << 
+        "valor guardado: " << *pont2 << endl;
+    }
+    
+    return 0;
+    
+}
+
 int main()
-{
+{   
+    
     int var1, var2;
-    int *pont1, *pont2;
     
     cin >> var1 >> var2;
     
-    pont1 = &var1;
-    pont2 = &var2;
-    
-    cout << pont1 << endl << pont2 << endl;
-    
-    if(pont1 > pont2){
-        cout << "O maior endereço é: " << pont1 << endl;
-    }
-    else{
-        cout << "O maior endereço é: " << pont2 << endl;
-    }
+    endereco(&var1,&var2);
 
     return 0;
 }
